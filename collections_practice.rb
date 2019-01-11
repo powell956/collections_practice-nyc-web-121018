63ae168b3ddd638b72f60a43126c61d5e1a31acb
powell956 <powell956@gmail.com>
2019-01-10 20:25:39 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,128 @@
+require 'pry'
 
+def sort_array_asc(int_array)
+  int_array.sort { |a, b| a <=> b }
+end
+
+
+def sort_array_desc(int_array)
+  int_array.sort { |a, b| b <=> a }
+end
+
+
+def sort_array_char_count(str_array)
+  str_array.sort { |str1, str2| str1.length <=> str2.length }
+end
+
+
+def swap_elements(array)
+  new_arr = []
+  counter = 0
+
+  while counter < array.length
+    if counter == 1
+      if array[1] > array [2]
+        new_arr << array[1]
+        new_arr << array[2]
+      else
+        new_arr << array[2]
+        new_arr << array[1]
+      end
+    elsif counter == 2
+      counter += 1
+      next
+    else
+      new_arr << array[counter]
+    end
+    counter += 1
+  end
+
+  new_arr
+end
+
+
+def swap_elements_from_to(array, index, destination_index)
+
+  new_arr = array.each.with_index do |el, i|
+    if i == index
+      array.insert(destination_index, el)
+      array.delete_at(index)
+    else
+      el
+    end
+  end
+
+  array
+end
+
+
+def reverse_array(array)
+  new_arr = []
+  counter = array.length - 1
+
+  while counter > -1
+    new_arr << array[counter]
+    counter -= 1
+  end
+
+  new_arr
+end
+
+
+def kesha_maker(array)
+  new_arr = []
+
+  for el in array do
+    if el.length > 2
+      chars = el.split("")
+
+      chars[2] = "$"
+      el = chars.join("")
+
+      new_arr << el
+    else
+      new_arr << el
+    end
+  end
+
+  new_arr
+end
+
+
+def find_a(array)
+  new_arr = []
+
+  for el in array do
+    if el.start_with?("a")
+      new_arr << el
+    end
+  end
+
+  new_arr
+end
+
+# def sum_array(array)
+#   sum = 0
+#
+#   for el in array do
+#     sum += el
+#   end
+#
+#   sum
+# end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.collect!.with_index do |element, index|
+    if index == 1
+      element
+    else
+      element + "s"
+    end
+    # binding.pry
+  end
+  array
+end
